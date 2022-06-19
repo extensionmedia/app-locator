72f2414917753abc950ead470accb0dcb13c9ee5
PC <elmeftouhi@extensionmedia.ma>
2022-06-19 23:04:23 +0100

    install fontawesome and tailwindcss

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+ mix.js('resources/js/app.js', 'public/js')
+    .sass('resources/css/fontawesome.scss', 'public/css/fontawesome.css')
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require("tailwindcss"),
+ ]);
